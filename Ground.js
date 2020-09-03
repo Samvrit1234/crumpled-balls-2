eba6c404c1a34047059b2f6a9f1d448608475d02
Samvrit1234 <68464137+Samvrit1234@users.noreply.github.com>
2020-09-03 12:31:09 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,23 @@
+class Ground{
+    constructor(x, y, width, height){
+   var options = {
+       isStatic : true,
+       restitution: 0.3,
+       density : 1.2
+   }
+
+       this.body = Bodies.rectangle(x, y,width,height);
+       this.width = width;
+       this.height = height;
+      
+
+       World.add(world,this.body);
+
+  
+    }
+    display(){
+       fill("yellow")
+       rectMode(CENTER);
+
+    }
+} 
---
+++ Paper.js
@@ -0,0 +1,22 @@
+class Paper{
+    constructor(x, y){
+   var options = {
+       isStatic : true,
+       restitution: 0.3,
+       density : 1.2
+   }
+
+       this.body = Bodies.circle(x, y,50,options);
+       this.image = loadImage("MatterJSBoilerPlate-master/paper.png");
+      
+
+       World.add(world,this.body);
+
+  
+    }
+    display(){
+       fill("pink")
+       ellipseMode(RADIUS);
+       ellipse(this.body.position.x,this.body.position.y,20,20);
+    }
+} 
